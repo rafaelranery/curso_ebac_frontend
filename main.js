3123afe664e7508a0fde5d13ea991baa903a5ebe
rafaelrn <rafaelrn60@gmail.com>
2023-02-21 17:41:11 -0300

    btn add activity // done activity style

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-alert('hi')
+$(document).ready(function() {
+    
+    
+    // btn adicionar atividade
+    $('button').click(function(e) {
+        e.preventDefault();
+        
+        let activityInput = $('#activity-input').val();
+
+        let newActivity = `<li>${activityInput}</li>`;
+        let activityList = $('ul');
+
+        $(newActivity).appendTo(activityList);
+
+        $('#activity-input').val('');
+    });
+
+    // Atividade feita estilização
+    $('ul').on('click', 'li', function() {
+        $(this).toggleClass('done')
+        // $(this) => seleciona o li em que disparou 
+    })
+
+})
