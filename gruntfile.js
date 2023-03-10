9eeb8d22e4ff4cfe94714107c2b151a501bb7b47
rafaelranery <rafaelrapnery@gmail.com>
2023-03-10 10:54:11 -0300

    watch function

--- gruntfile.js
+++ gruntfile.js
@@ -1,11 +1,42 @@
 module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-    
+        less: {
+            development: {
+                files: {
+                    'dev/styles/main.css':'src/styles/main.less'
+                }
+            },
+            production: {
+                options: {
+                    compress: true,
+                },
+                files: {
+                    'dist/styles/main.min.css':'src/styles/main.less'
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: 'src/styles/**/*.less',
+                tasks: ['less:development']
+            }
+        }
     
     });
-    
-    grunt.registerTask('olaGrunt', function() {
-        return console.log('olá grunt');
+    grunt.registerTask('watch', function() {
+
     })
+    // grunt.registerTask('olaGrunt', function() {
+    //     return console.log('olá grunt');
+    // })
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+
+    grunt.registerTask('lessCompiler', ['less']);
+
+    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('build', ['less:production']);
 };
